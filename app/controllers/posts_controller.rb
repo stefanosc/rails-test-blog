18039722df8e97eeb10d63bdf6e23715f84b00b1
Stefano Schiavi <stefano@Stefanos-MacBook-Pro.local>
2013-11-27 23:39:59 -0800

    show all posts v 1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,17 +6,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
-  def create
-    @post = Post.new(params[:post])
-    @post.save
 
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+   
+    @post.save
     redirect_to @post
   end
+   
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 
-private
-  def post_params
-    params.require(:post).permit(:title, :text)
-  end
 
 end
