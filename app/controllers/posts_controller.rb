d266e54ccf087b2bd9907a5090ace4c813b2cacf
Stefano Schiavi <stefano@bvprojects.org>
2013-11-29 12:29:40 -0800

    updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,36 @@
 class PostsController < ApplicationController
   
   def new
+    @post = Post.new
   end
   
   def show
     @post = Post.find(params[:id])
   end
-  
+
+  def index
+    @posts = Post.all
+  end
+
   def create
-    @post = Post.new(params[:post])
-    @post.save
-
-    redirect_to @post
+    @post = Post.new(post_params)
+   
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+    
   end
 
-private
-  def post_params
-    params.require(:post).permit(:title, :text)
+  def edit
+    @post = Post.find(params[:id])
   end
+   
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+
 
 end
