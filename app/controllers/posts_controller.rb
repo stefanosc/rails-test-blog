a00b37909ec16c7b009c324436bf3ccfecba4692
Stefano Schiavi <stefano@bvprojects.org>
2013-11-29 12:38:28 -0800

    updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,12 +11,6 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-   
-  private
-    def post_params
-      params.require(:post).permit(:title, :text)
-    end
-
 
   def create
     @post = Post.new(post_params)
